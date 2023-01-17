d66d1d97aa04c7374935222491fdea957cb6352e
HubaBoy <101318913+HubaBoy@users.noreply.github.com>
2023-01-17 21:00:43 +0200

    Add flapyBird

---
+++ Assets/LogicScript.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class LogicScript : MonoBehaviour
+{
+    public int playerScore;
+    public Text scoreText;
+    public GameObject GameOverScreen;
+
+   
+    [ContextMenu("Increase score")]
+    public void addScore( int scoreToAdd)
+    {
+         playerScore = playerScore + scoreToAdd;
+         scoreText.text= playerScore.ToString();     
+    }
+    
+    public void GameRest()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+    
+    public void GameOver()
+    {
+        GameOverScreen.SetActive(true);
+    }
+}
