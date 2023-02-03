eeacd2d1a0558758acf8e70ef10d72092b2f354b
HubaBoy <101318913+HubaBoy@users.noreply.github.com>
2023-02-03 20:27:36 +0200

    Score Update

--- Assets/pipemiddlescript.cs
+++ Assets/pipemiddlescript.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class pipemiddlescript : MonoBehaviour
 {
     public LogicScript logic;
+    public Birdscript bird;
     // Start is called before the first frame update
      void Start()
     {
         logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicScript>();
+       bird = GameObject.FindGameObjectWithTag("bird").GetComponent<Birdscript>();
     }
 
     // Update is called once per frame
@@ -19,8 +21,10 @@ public class pipemiddlescript : MonoBehaviour
     }
       private void OnTriggerEnter2D(Collider2D collider)
      {
-        
+        if(bird.birdlife == true
+        ){
          logic.addScore(1);
+        }
   
     }
 }
